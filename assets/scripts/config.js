124c769b60dee452c149172e58fccb7898370975
JLisaW <j.lisa.wang@gmail.com>
2017-04-11 09:50:13 -0400

    project practice

---
+++ assets/scripts/config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    production: 'https://ga-wdi-boston.herokuapp.com',
+    development: 'http://localhost:4741'
+  }
+}
+
+module.exports = config
+
+config.apiOrigins.development
+config.apiOrigins.production
