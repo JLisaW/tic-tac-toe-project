1b1bd0d6249aa0de90e031dcac93e8e6f883b728
JLisaW <j.lisa.wang@gmail.com>
2017-04-13 17:10:50 -0400

    created logic, created board, added click   handler

--- assets/scripts/game-engine/events.js
+++ assets/scripts/game-engine/events.js
@@ -1,20 +1,42 @@
 'use strict'
 
 const gamesApi = require('../game-engine/api.js')
-const gamesUi = require('./ui.js')
+const gamesUi = require('../game-engine/ui.js')
+// const checkWin = require('../game-logic/logic.js')
+// const getFormFields = require(`../../../lib/get-form-fields`)
+
+let player = 'x'
+
+const togglePlayer = function () {
+  if (player === 'x') {
+    player = 'o'
+  } else {
+    player = 'x'
+  }
+}
 
 const onNewGame = function (event) {
   event.preventDefault()
-
   gamesApi.create()
     .then(gamesUi.onSuccess)
     .catch(gamesUi.onError)
 }
 
-const addHandlers = () => {
-  $('#new-game').on('submit', onNewGame)
+const onClickSquare = function () {
+  event.preventDefault()
+  if (player === 'x') {
+    $(this).text('X')
+  } else {
+    $(this).text('O')
+  }
+  // $(this).attr("id")
+  togglePlayer()
+}
+const addGameHandler = function () {
+  $('.square').on('click', onClickSquare)
 }
 
 module.exports = {
-  addHandlers
+  onNewGame,
+  addGameHandler
 }
---
+++ assets/scripts/game-logic/game.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const game = {
+  xTurn: true,
+  currentMove: 0,
+  currentBox: 0
+}
+
+module.exports = {
+  game
+}
